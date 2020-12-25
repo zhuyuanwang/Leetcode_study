1700dc5f2d680a27694385e3d9b4e0dc022d2802
unknown <sw00380@shuwei.com>
2020-12-25 15:48:22 +0800

    commit

--- README.md
+++ README.md
@@ -22,3 +22,31 @@ def kidsWithCandies(self, candies, extraCandies):
     return result_list
 ```
 
+时间：2020年12月25日15:45:22
+
+#### 118. 拥有最多糖果的孩子
+
+原题链接：https://leetcode-cn.com/problems/pascals-triangle/
+
+```python3
+def generate(self, numRows):
+    """
+
+    :param numRows:
+    :return:
+    """
+    resultlist = []
+    for i in range(numRows):
+        resultlist.append([])
+        for x in range(i + 1):
+            if i <= 1:
+                resultlist[i].append(1)
+            else:
+                resultlist[i].append('')
+                if x == 0 or x == i:
+                    resultlist[i][x] = 1
+                else:
+                    resultlist[i][x] = resultlist[i - 1][x - 1] + resultlist[i - 1][x]
+    return resultlist
+```
+
--- leetcode.py
+++ leetcode.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-
+# https://leetcode-cn.com/problemset/algorithms/
 
 class Solution:
     def kidsWithCandies(self, candies, extraCandies):
@@ -17,8 +17,27 @@ class Solution:
                 result_list.append(False)
         return result_list
 
+    def generate(self, numRows):
+        """
+
+        :param numRows:
+        :return:
+        """
+        resultlist = []
+        for i in range(numRows):
+            resultlist.append([])
+            for x in range(i + 1):
+                if i <= 1:
+                    resultlist[i].append(1)
+                else:
+                    resultlist[i].append('')
+                    if x == 0 or x == i:
+                        resultlist[i][x] = 1
+                    else:
+                        resultlist[i][x] = resultlist[i - 1][x - 1] + resultlist[i - 1][x]
+        return resultlist
+
 if __name__ == '__main__':
     s = Solution()
-    candies = [2, 3, 5, 1, 3]
-    extraCandies = 3
-    print(s.kidsWithCandies(candies, extraCandies))
+    numRows = 8
+    print(s.generate(numRows))
